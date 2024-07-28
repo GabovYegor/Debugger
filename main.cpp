f08597c5a0df256bb835a1f9611643a6163f0e02
GabovYegor <gabov.egor2017@yandex.ru>
2024-07-28 12:46:38 +0200

    Refactoring #3

--- main.cpp
+++ main.cpp
@@ -8,118 +8,58 @@
 #include <iostream>
 #include <vector>
 #include <capstone/capstone.h>
+#include <utility>
 
 constexpr size_t max_instruction_size = 15;
 constexpr size_t number_of_instructions_to_show = 20;
 
-std::vector<uint8_t> hexStringToBytes(const std::string& hex) {
-    std::vector<uint8_t> bytes;
-    for (size_t i = 0; i < hex.length(); i += 2) {
-        std::string byteString = hex.substr(i, 2);
-        uint8_t byte = (uint8_t)strtol(byteString.c_str(), nullptr, 16);
-        bytes.push_back(byte);
-    }
-    return bytes;
-}
-
-cs_insn disassemble_one_instruction(const std::vector<uint8_t>& code) {
-    csh handle;
-    cs_insn *insn;
-
-    if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) {
-        std::cerr << "Failed to initialize Capstone" << std::endl;
+class Utilities {
+public:
+    static std::vector<uint8_t> string_to_bytes(const std::string& hex) {
+        std::vector<uint8_t> bytes;
+        for (size_t i = 0; i < hex.length(); i += 2) {
+            std::string byteString = hex.substr(i, 2);
+            uint8_t byte = (uint8_t)strtol(byteString.c_str(), nullptr, 16);
+            bytes.push_back(byte);
+        }
+        return bytes;
     }
 
-    size_t count = cs_disasm(handle, code.data(), code.size(), 0x1, 1, &insn);
-    if (count > 0) {
-        cs_close(&handle);
-        return insn[0];
-    }
-    std::cerr << "Failed to disassemble given code!" << std::endl;
-}
+    static std::pair<cs_insn*, size_t> disassemble(
+        const std::vector<uint8_t>& code, const long address,
+        const int number_of_instructions_to_disassemble) {
 
-void disassemble(const std::vector<uint8_t>& code, const long address) {
-    csh handle;
-    cs_insn *insn;
-    size_t count;
+        csh handle;
+        cs_insn *insn;
 
-    if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) {
-        std::cerr << "Failed to initialize Capstone" << std::endl;
-        return;
-    }
-
-    count = cs_disasm(handle, code.data(),
-                      code.size(), address, number_of_instructions_to_show, &insn);
-    if (count > 0) {
-        for (size_t i = 0; i < count; i++) {
-            std::cout << "0x" << std::hex << insn[i].address << ":\t";
-            std::cout << insn[i].mnemonic << "\t" << insn[i].op_str << std::endl;
+        if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) {
+            std::cerr << "Failed to initialize Capstone" << std::endl;
+            return { nullptr, 0 };
         }
 
-        cs_free(insn, count);
-    } else {
+        const auto count = cs_disasm(handle, code.data(), code.size(), address,
+                                     number_of_instructions_to_disassemble, &insn);
+        if (count > 0) {
+            // TODO: Free insn. Unique_ptr???
+            // cs_free(insn, count);
+            cs_close(&handle);
+            return { insn, count };
+        }
         std::cerr << "Failed to disassemble given code!" << std::endl;
+        return { nullptr, 0 };
     }
 
-    cs_close(&handle);
-}
-
-long int swapBytes64(long value) {
-    return ((value >> 56) & 0x00000000000000FF) |
-           ((value >> 40) & 0x000000000000FF00) |
-           ((value >> 24) & 0x0000000000FF0000) |
-           ((value >> 8)  & 0x00000000FF000000) |
-           ((value << 8)  & 0x000000FF00000000) |
-           ((value << 24) & 0x0000FF0000000000) |
-           ((value << 40) & 0x00FF000000000000) |
-           ((value << 56) & 0xFF00000000000000);
-}
-
-std::string get_RIP_data(pid_t child_pid, long& address) {
-    user_regs_struct regs;
-    ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
-
-    std::vector<long> rip_data;
-    size_t bytes_read = 0;
-    for(int i = 0; i < number_of_instructions_to_show *
-                       max_instruction_size; ++i) {
-        long instr = ptrace(PTRACE_PEEKTEXT, child_pid, regs.rip + i * sizeof(long), 0);
-        if(instr == 0) {
-            break;
-        }
-        rip_data.emplace_back(instr);
+    static long swapBytes64(long value) {
+        return ((((value) & 0xff00000000000000l) >> 56) |
+                    (((value) & 0x00ff000000000000l) >> 40) |
+                    (((value) & 0x0000ff0000000000l) >> 24) |
+                    (((value) & 0x000000ff00000000l) >> 8 ) |
+                    (((value) & 0x00000000ff000000l) << 8 ) |
+                    (((value) & 0x0000000000ff0000l) << 24) |
+                    (((value) & 0x000000000000ff00l) << 40) |
+                    (((value) & 0x00000000000000ffl) << 56));
     }
-
-    std::ostringstream os;
-    for(const auto it : rip_data) {
-        os << std::hex << std::setfill('0')
-           << std::setw(sizeof(long int) * 2) << swapBytes64(it);
-    }
-    std::string rip_commands = os.str();
-    rip_commands.erase(rip_commands.find_last_not_of('0') + 1);
-    if(rip_commands.size() % 2) {
-        rip_commands.push_back('0');
-    }
-    address = regs.rip;
-    return rip_commands;
-}
-
-cs_insn get_next_instruction_info(pid_t child_pid) {
-    long address = 0;
-    return disassemble_one_instruction(hexStringToBytes(get_RIP_data(child_pid, address)));
-}
-
-void print_near_code(pid_t child_pid) {
-    long address = 0;
-    const auto code = hexStringToBytes(get_RIP_data(child_pid, address));
-    disassemble(code, address);
-}
-
-class Printer {
-
 };
-
-
 
 class Debugger {
     struct Breakpoint {
@@ -143,6 +83,47 @@ class Debugger {
         ShowBreakPoints,
         InvalidCommand
     };
+
+    void print_near_code() {
+        const auto [instructions, number_of_instr] = Utilities::disassemble(
+            get_next_instructions(number_of_instructions_to_show), get_RIP(), 20);
+
+        for (size_t i = 0; i < number_of_instr; i++) {
+            std::cout << "0x" << std::hex << instructions[i].address << ":\t";
+            std::cout << instructions[i].mnemonic << "\t" << instructions[i].op_str << std::endl;
+        }
+    }
+
+    unsigned long long get_RIP() {
+        user_regs_struct regs {};
+        ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
+        return regs.rip;
+    }
+
+    std::vector<uint8_t> get_next_instructions(const size_t max_number_of_instructions_to_read) {
+        const auto rip_value = get_RIP();
+        std::vector<long> rip_data;
+        for(int i = 0; i < max_number_of_instructions_to_read *
+                           max_instruction_size; ++i) {
+            long instr = ptrace(PTRACE_PEEKTEXT, child_pid, rip_value + i * sizeof(long), 0);
+            if(instr == 0) {
+                break;
+            }
+            rip_data.emplace_back(instr);
+        }
+
+        std::ostringstream os;
+        for(const auto it : rip_data) {
+            os << std::hex << std::setfill('0')
+               << std::setw(sizeof(long int) * 2) << Utilities::swapBytes64(it);
+        }
+        std::string rip_commands = os.str();
+        rip_commands.erase(rip_commands.find_last_not_of('0') + 1);
+        if(rip_commands.size() % 2) {
+            rip_commands.push_back('0');
+        }
+        return Utilities::string_to_bytes(rip_commands);
+    }
 
     UserCommands convert_user_input_to_UserCommands(const std::string& user_input) {
         if(user_input == "bp") {
@@ -197,7 +178,7 @@ class Debugger {
         remove_breakpoint(child_pid, *bp_to_remove);
         break_points.erase(bp_to_remove);
 
-        print_near_code(child_pid);
+        print_near_code();
     }
 
     void user_set_breakpoint() {
@@ -251,19 +232,20 @@ class Debugger {
         if(!WIFSTOPPED(wait_status))
             return 1;
 
-        print_near_code(child_pid);
+        print_near_code();
         return 0;
     }
 
     int step_over() {
-        const auto next_instruction_info = get_next_instruction_info(child_pid);
+        const auto [next_instruction_info, n] =
+            Utilities::disassemble(get_next_instructions(1), get_RIP(), 1);
 
-        if(next_instruction_info.mnemonic == "call") {
-            user_regs_struct regs;
+        if(next_instruction_info[0].mnemonic == "call") {
+            user_regs_struct regs {};
             ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
 
             Breakpoint temp_break_point;
-            temp_break_point.addr = regs.rip + next_instruction_info.size;
+            temp_break_point.addr = regs.rip + next_instruction_info[0].size;
 
             create_breakpoint(temp_break_point);
             break_points.emplace_back(temp_break_point);
@@ -298,7 +280,7 @@ public:
 
         define_child_process_return_address();
 
-        print_near_code(child_pid);
+        print_near_code();
 
         std::string user_input;
 
